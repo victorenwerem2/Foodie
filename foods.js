2cf757c874222bc2159b655d9157ef42494cffb1
victorenwerem2 <victorenwerem2@gmail.com>
2026-03-28 14:22:48 -0500

    First Push

---
+++ foods.js
@@ -0,0 +1,47 @@
+const foods = [
+
+{
+name:"Classic Burger",
+price:12,
+image:"https://images.unsplash.com/photo-1600891964599-f61ba0e24092",
+description:"Juicy grilled beef burger with lettuce, tomato, and cheese."
+},
+
+{
+name:"Italian Pizza",
+price:15,
+image:"https://images.unsplash.com/photo-1565299624946-b28f40a0ae38",
+description:"Wood-fired pizza topped with mozzarella and basil."
+},
+
+{
+name:"Creamy Pasta",
+price:14,
+image:"https://images.unsplash.com/photo-1540189549336-e6e99c3679fe",
+description:"Delicious Alfredo pasta made with parmesan cheese."
+},
+
+{
+name:"Grilled Steak",
+price:20,
+image:"https://images.unsplash.com/photo-1550547660-d9450f859349",
+description:"Tender grilled steak seasoned with herbs."
+},
+
+{
+name:"Fresh Salad",
+price:10,
+image:"https://images.unsplash.com/photo-1604908176997-125f25cc6f3d",
+description:"Healthy green salad with avocado dressing."
+},
+
+
+
+{
+name:"Chocolate Dessert",
+price:9,
+image:"https://images.unsplash.com/photo-1551024601-bec78aea704b",
+description:"Rich chocolate dessert topped with whipped cream."
+}
+
+];
